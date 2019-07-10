d01a6182142f1b1043a6de8f49199f52c0f92f6b
Tomasz-Szkonter <44675487+Tomasz-Szkonter@users.noreply.github.com>
2019-07-10 02:01:20 +0200

    Add files via upload

---
+++ script/main1.js
@@ -0,0 +1,203 @@
+
+
+
+// /* **********************************************
+// **
+// ** UI Elements Module
+// **
+// ** - this module will be responsible for controling UI Elements like 'menu'
+// ** ******************************************** */
+
+const UI = (function () {
+    let menu = document.querySelector("#menu-container");
+
+    // show the app and hide the loading screen
+    const showApp = () => {
+        document.querySelector("#app-loader").classList.add('display-none');
+        document.querySelector("main").removeAttribute('hidden');
+    };
+
+    // hide the app and show the loading screen
+    const loadApp = () => {
+        document.querySelector("#app-loader").classList.remove('display-none');
+        document.querySelector("main").setAttribute('hidden', 'true');
+    };
+
+    // show menu
+    const _showMenu = () => menu.style.right = 0;
+
+    // hide menu
+    const _hideMenu = () => menu.style.right = '-65%';
+
+    const _toggleHourlyWeather = () => {
+        let hourlyWeather = document.querySelector("#hourly-weather-wrapper"),
+            arrow = document.querySelector("#toggle-hourly-weather").children[0],
+            visible = hourlyWeather.getAttribute('visible'),
+            dailyWeather = document.querySelector("#daily-weather-wrapper");
+
+        if (visible == 'false') {
+            hourlyWeather.setAttribute('visible', 'true');
+            hourlyWeather.style.bottom = 0;
+            arrow.style.transform = "rotate(180deg)";
+            dailyWeather.style.opacity = 0;
+        } else if (visible == 'true') {
+            hourlyWeather.setAttribute('visible', 'false');
+            hourlyWeather.style.bottom = '-100%';
+            arrow.style.transform = "rotate(0deg)";
+            dailyWeather.style.opacity = 1;
+        } else console.error("Unknown state of the hourly weather panel and visible attribute");
+    };
+
+    const drawWeatherData = (data, location) => {
+
+        let currentlyData = data.currently;
+
+
+        // set current weather
+        // ===================
+        // set current location
+        document.querySelectorAll(".location-label").forEach((e) => {
+            e.innerHTML = location;
+        });
+        // set the background
+        document.querySelector('main').style.backgroundImage = `url("./assets/images/bg-images/${currentlyData.icon}.jpg")`;
+        // set the icon
+        document.querySelector("#currentlyIcon").setAttribute('src', `./assets/images/summary-icons/${currentlyData.icon}-white.png`);
+        // set summary
+        document.querySelector("#summary-label").innerHTML = currentlyData.summary;
+        // set temperature from Fahrenheit -> Celcius
+        document.querySelector("#degrees-label").innerHTML = Math.round((
+            currentlyData.temperature - 32) * 5 / 9) + '&#176;'
+
+        // set humidty
+        document.querySelector("#humidity-label").innerHTML = Math.round(currentlyData.humidity * 100) + '%';
+        // set wind speed
+        document.querySelector("#wind-speed-label").innerHTML = (currentlyData.windSpeed * 1.6093).toFixed(1) + ' kph';
+
+        UI.showApp();
+    };
+
+    // menu events
+    document.querySelector("#open-menu-btn").addEventListener('click', _showMenu);
+    document.querySelector("#close-menu-btn").addEventListener('click', _hideMenu);
+
+    // hourly-weather wrapper event
+    document.querySelector("#toggle-hourly-weather").addEventListener('click', _toggleHourlyWeather);
+
+    // export
+    return {
+        showApp,
+        loadApp,
+        drawWeatherData
+    }
+
+})();
+
+
+// /* **********************************************
+// **
+// ** Get location Module
+// **
+// ** - this module will be responsible for getting the data about the location to search for weather
+// ** ******************************************** */
+
+const GETLOCATION = (function () {
+
+    let location;
+
+    const locationInput = document.querySelector("#location-input"),
+        addCityBtn = document.querySelector("#add-city-btn");
+
+
+    const _addCity = () => {
+        location = locationInput.value;
+        locationInput.value = "";
+        addCityBtn.setAttribute('disabled', 'true');
+        addCityBtn.classList.add('disabled');
+
+        // get weather data
+        WEATHER.getWeather(location)
+    }
+
+    locationInput.addEventListener('input', function () {
+        let inputText = this.value.trim();
+
+        if (inputText != '') {
+            addCityBtn.removeAttribute('disabled');
+            addCityBtn.classList.remove('disabled');
+        } else {
+            addCityBtn.setAttribute('disabled', 'true');
+            addCityBtn.classList.add('disabled');
+        }
+    })
+
+    addCityBtn.addEventListener('click', _addCity);
+})();
+
+
+
+/* **********************************************
+**
+** Get Weather data
+**
+// ** - this module will aquire weather data and then it will pass to another module which will put the data on UI
+// ** ******************************************** */
+
+const WEATHER = (function () {
+
+  const darkSkyKey ='a01d420cb15322898f3ca3f58a4acfd6',
+    geocoderKey ='a54c8cb5db7b4a4cb7e22c44bfeca43a';
+
+
+  // return a valid URL for OpenCage API
+  const _getGeocodeURL = (location) =>
+  `https://api.opencagedata.com/geocode/v1/json?q=${location}&key=${geocoderKey}`;
+
+  // return a valid URL for DarkSky API
+  const _getDarkSkyURL = (lat,lng) =>
+  `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${darkSkyKey}/${lat},${let}`;
+    const _getDarkSkyData = (url,location) => {
+        axios.get(url)
+            .then( (res) => {
+                console.log(res);
+                UI.drawWeatherData(res.data, location)
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+    };
+
+    const getWeather = (location) => {
+        UI.loadApp();
+
+        let geocodeURL = _getGeocodeURL(location);
+
+        axios.get(geocodeURL)
+            .then( (res) => {
+                let lat = res.data.results[0].geometry.lat,
+                    lng = res.data.results[0].geometry.lng;
+
+                let darkskyURL = _getDarkSkyURL(lat,lng);
+
+                _getDarkSkyData(darkskyURL,location);
+            })
+            .catch( (err) => {
+                console.log(err)
+            })
+    };
+
+    return{
+        getWeather
+    }
+})();
+
+// /* **********************************************
+// **
+// ** Init
+// **
+// **
+// ** ******************************************** */
+
+window.onload = function () {
+    UI.showApp();
+}
